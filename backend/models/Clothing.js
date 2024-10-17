1d6d0d561204072396eec36d0407f72fb72b4bf6
zeeshankhan-05 <khanzeeshan1205@gmail.com>
2024-10-16 20:22:59 -0500

    Clothing

--- backend/models/Clothing.js
+++ backend/models/Clothing.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
-
 const ClothingSchema = new mongoose.Schema({
   type: { type: String, required: true },
   color: { type: String, required: true },
-  occasion: { type: String, enum: ['Casual', 'Formal', 'Work', 'Gym', 'Party'] },
-  weatherCategory: { type: String, enum: ['Hot', 'Warm', 'Cold', 'Freezing', 'Rainy', 'Snowy'], default: 'Warm' },
+  occasion: { type: String, enum: ['Casual', 'Formal', 'Party', 'Work'], default: 'Casual' },
+  weatherCategory: { type: String, enum: ['Warm', 'Cold', 'Rainy'], default: 'Warm' },
   status: { type: String, enum: ['Clean', 'Dirty'], default: 'Clean' },
-  imageUrl: String,    // URL to the clothing image
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }  // Foreign key to the user who owns the clothing
+  imageUrl: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 module.exports = mongoose.model('Clothing', ClothingSchema);
